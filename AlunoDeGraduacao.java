ee1624b4f18d25e4f18b1c3db5c8cd7c5792f2cb
Cadub <99202990+Cadub@users.noreply.github.com>
2022-06-08 17:48:29 -0300

    Update AlunoDeGraduacao.java

--- AlunoDeGraduacao.java
+++ AlunoDeGraduacao.java
@@ -1,5 +1,8 @@
 package Heranca3;
 
+//AlunoDeGraduacao É-UM Pessoa
+//extends: ele herda de outro arquivo, necessario tem uma relação lógica, por exemplo, 
+
 public class AlunoDeGraduacao extends Aluno {
     double nota1, nota2, notaFinal;
 }
