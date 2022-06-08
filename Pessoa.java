14f1ca6a5614e6c133bfcb5192b60b0c495b7508
Cadub <99202990+Cadub@users.noreply.github.com>
2022-06-08 17:49:43 -0300

    Update Pessoa.java

--- Pessoa.java
+++ Pessoa.java
@@ -1,5 +1,15 @@
 package Heranca3;
 
+//Bloco de cogio que entra em execução quando tem um objeto = Construtor
+/*Exemplo: public Pessoa() {
+    System.out.println("Construindo uma pessoa...")
+}*/
+/*Construtor padrão: tem a lista de parametros vazia
+public Pessoa(){
+
+}*/
+
+
 public abstract class Pessoa {
     String nome;
     int idade;
